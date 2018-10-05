4558cc17a7caece60650b5d9a71dcb67ed7d8d99
Moshe Immerman <moshe.immerman@vitalitygroup.com>
2018-10-05 07:31:49 +0200

    fix cli

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name = 'ansible-deploy', version = '0.1',
+    name = 'ansible-deploy', version = '0.2',
     install_requires=['ansible-run'],
     url = 'https://www/github.com/moshloop/ansible-deploy',
     author = 'Moshe Immerman', author_email = 'firstname.surname@gmail.com',
