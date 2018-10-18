841740a1c1d4f728d2c5efecdb96096a20fca049
Moshe Immerman <moshe.immerman@vitalitygroup.com>
2018-10-18 21:56:30 +0200

    fix pip installer

--- setup.py
+++ setup.py
@@ -4,21 +4,19 @@ from setuptools.command.install import install
 from ansible_deploy import __version__
 import os
 from os.path import isfile
-import pathspec
+
+
 __name__ = 'ansible-deploy'
+role_name = __name__.split('-')[1]
+base = '/etc/ansible/roles/%s' % role_name
 cwd = os.getcwd()
-base = '/etc/ansible/roles/%s' % __name__.split('-')[1]
 data_files = []
 
-spec = None
-
-if os.path.isfile('.gitignore'):
-  spec = pathspec.PathSpec.from_lines('gitwildmatch',open('.gitignore').read().splitlines())
 for dir in ['library', 'meta', 'filter_plugins', 'templates', 'defaults', 'tasks']:
   _files = []
   for root, dirs, files in os.walk(dir, topdown=False):
    for name in files:
-      if (spec != None and spec.match_file(os.path.join(root, name))) or name.startswith('.') or root.startswith('./.'):
+      if name.startswith('.') or root.startswith('./.'):
         continue
       _files.append("%s/%s" % (root,name))
   data_files.append(("%s/%s" % (base, dir), _files))
@@ -26,16 +24,16 @@ for dir in ['library', 'meta', 'filter_plugins', 'templates', 'defaults', 'tasks
 class link_role(install):
     def run(self):
         install.run(self)
-        dist = "%s/%s/%s" % (os.getcwd(), self.install_data,self.config_vars['dist_name'])
+        dist = "%s/%s/%s" % (os.getcwd(), self.install_data, self.config_vars['dist_name'])
         role = "/etc/ansible/roles/%s" % role_name
-        print ("Renaming %s to %s" % (dist,role))
+        print ("Renaming %s to %s" % (dist, role))
         shutil.rmtree(role)
-        os.renames(dist,role)
+        os.renames(dist, role)
 
 setup(
     name = __name__,
     version = __version__,
-    install_requires = 'ansible-extras',
+    install_requires = ['ansible-extras'],
     packages = [__name__.replace("-", "_")],
     data_files = data_files,
     cmdclass = {'install': link_role},
