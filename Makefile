22c2678faa12046c211c98331f576af14ef3b6f1
Moshe Immerman <moshe.immerman@vitalitygroup.com>
2018-10-19 10:34:20 +0200

    switch to rpm/deb packaging

---
+++ Makefile
@@ -0,0 +1,17 @@
+NAME := "ansible-deploy"
+VERSION := $(shell git tag --points-at HEAD )
+
+ifdef VERSION
+else
+  VERSION := $(shell git describe --abbrev=0 --tags)-debug
+endif
+
+
+.PHONY: package
+package:
+	$(shell rm *.rpm || true)
+	$(shell rm *.deb || true)
+	docker run --rm -it -v $(CURDIR):$(CURDIR) -w $(CURDIR) alanfranz/fpm-within-docker:ubuntu-xenial fpm  -s dir -t deb -n $(NAME) -v $(VERSION) -x "*.DS_Store"/ -x ".git" ./=/etc/ansible/roles/extras
+	mv *.deb $(NAME).deb
+	docker run --rm -it -v $(CURDIR):$(CURDIR) -w $(CURDIR) alanfranz/fpm-within-docker:centos-7 fpm  -s dir -t rpm -n $(NAME) -v $(VERSION) -x "*.DS_Store" -x ".git" ./=/etc/ansible/roles/extras
+	mv *.rpm $(NAME).rpm
--- setup.py
+++ setup.py
@@ -3,49 +3,14 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 from ansible_deploy import __version__
 import os
-import shutil
-from os.path import isfile
 
-
-__name__ = 'ansible-deploy'
-role_name = __name__.split('-')[1]
-base = '/etc/ansible/roles/%s' % role_name
-cwd = os.getcwd()
-data_files = []
-
-for dir in ['library', 'meta', 'filter_plugins', 'templates', 'defaults', 'tasks']:
-  _files = []
-  for root, dirs, files in os.walk(dir, topdown=False):
-   for name in files:
-      if name.startswith('.') or root.startswith('./.'):
-        continue
-      _files.append("%s/%s" % (root,name))
-  data_files.append(("%s/%s" % (role_name, dir), _files))
-
-class link_role(install):
-    def run(self):
-        install.run(self)
-        dist = self.install_data + "/" + role_name
-        if not dist.startswith('/'):
-          dist = "%s/%s" % (os.getcwd(), dist)
-        role = "/etc/ansible/roles/%s" % role_name
-        if os.path.isdir(dist):
-          print ("Renaming %s to %s" % (dist, role))
-          if os.path.isdir(role):
-            print ("cleaning %s" % role)
-            import shutil
-            shutil.rmtree(role)
-          check_output("mkdir -p /etc/ansible/roles", shell=True)
-          os.renames(dist, role)
+__name__ = "ansible-deploy"
 
 setup(
     name = __name__,
     version = __version__,
-    install_requires = ['ansible-extras'],
     scripts = ['ansible-deploy'],
     packages = [__name__.replace("-", "_")],
-    data_files = data_files,
-    cmdclass = {'install': link_role},
     url = 'https://www/github.com/moshloop/ansible-deploy',
     author = 'Moshe Immerman', author_email = 'moshe.immerman@gmail.com'
 )
